558d180dd9ee1257a07a6080d1be566d79ee06b0
Andrew Silva <andrewsilva9@gmail.com>
2019-05-03 01:23:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,8 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
   end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
