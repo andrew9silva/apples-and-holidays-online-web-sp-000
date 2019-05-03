709f511476e331456e057a867548766d62ac7853
Andrew Silva <andrewsilva9@gmail.com>
2019-05-03 01:05:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash[:fall] << thanksgiving_eve
+  holiday_hash[:fall] << 
   # code here
   # remember to return the updated hash
 
