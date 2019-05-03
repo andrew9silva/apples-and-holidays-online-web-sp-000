9ad9836571d0fc10fb4256d2abce6a6600273bee
Andrew Silva <andrewsilva9@gmail.com>
2019-05-03 01:26:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
       supplies = supplies.join(", ")
-      holiday = holiday.to_s.split("_").map { |w| w.capitalize.join(" ")<< ":"  }
+      holiday = holiday.to_s.split("_").map { |w| w.capitalize}.join(" ")<< ":"
   end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
